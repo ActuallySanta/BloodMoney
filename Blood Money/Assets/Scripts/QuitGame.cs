c57a4e41a458b57acf8f4f8686bb48b40d615a65
ActuallySanta <SantiBenitezFer@outlook.com>
2024-10-29 15:40:29 -0400

    Making a new level for the new aspect ration

--- Blood Money/Assets/Scripts/QuitGame.cs
+++ Blood Money/Assets/Scripts/QuitGame.cs
@@ -4,20 +4,9 @@ using UnityEngine;
 
 public class QuitGame : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void doExitGame()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {   
-    }
-
-    public void doExitGame() { 
         Debug.Log("losies this quit");
         Application.Quit();
-
     }
 }
